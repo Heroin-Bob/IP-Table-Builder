4573ab00b62ddf02f6928062a4dbdbac2d36d68a
Thomas Patton <thomaspatton2010@yahoo.com>
2020-03-06 21:49:33 -0600

    030620-2149

--- IPTableBuilder/Form1.Designer.cs
+++ IPTableBuilder/Form1.Designer.cs
@@ -318,6 +318,7 @@
             // 
             // button1
             // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(888, 368);
             this.button1.Name = "button1";
